46eb7cdc553bbf486e636d7eb61097cdc66b7174
theostowell <theo@fundamentalised.com>
2026-08-20 12:51:01 +0100

    Install Whop Pixel globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -96,6 +96,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en" className={`light ${GeistMono.variable} ${cormorantGaramond.variable}`}>
+      <head dangerouslySetInnerHTML={{ __html: `<script>!function(w,d,s,u,n,a,b){if(w[n])return;a=w[n]={q:[],t:+new Date,s:[],o:u,track:function(){a.q.push([+new Date].concat([].slice.call(arguments)))},setScope:function(){a.s=[].slice.call(arguments).filter(function(x){return typeof x==="string"});a.q.push([+new Date,"setScope"].concat(a.s))},scope:function(){var c=[].slice.call(arguments);return{track:function(){a.q.push([+new Date].concat([].slice.call(arguments)).concat([{__scope:c}]))}}}};b=d.createElement(s);b.async=1;b.src=u+"/s.js";d.getElementsByTagName(s)[0].parentNode.insertBefore(b,d.getElementsByTagName(s)[0])}(window,document,"script","https://t.whop.tw","whop");whop.setScope("biz_tGIL6R2J3Z0k5p");whop.track("page");</script>` }} />
       <body>
         <AuthModalProvider>
           <SidebarProvider>
